4cde9c42697fe5fd73b671ce4d0562dadbaa6032
MigueBogs <m_plqp2@hotmail.com>
2019-06-11 18:07:37 -0500

    Edicion en fecha
    *Editar fecha y hora (Pickers)

--- JS/StaticText.js
+++ JS/StaticText.js
@@ -1,7 +1,52 @@
+var texto = {"numero":"1"};
 
 
-var texto = {"fijo1":"Juan"};
+var output = document.getElementById('Number');
+output.innerHTML = texto.numero;
+
+/*var infoGen={"hora": "17:30 (22:30 GMT)", 
+			"coords": "28.8°Norte 68.7° Oeste.",
+			"loc": "A 540 km al suroeste de Bermuda y a 2,010 km al este-noreste de las costas de Quintana Roo."
+			"despl": "Hacia el norte (350°) a 22km/h."
+			"viento": "65 km/h."
+			"racha":"85 km/h."};*/
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+	    xobj.open('GET', 'JS/info.json', true); // Replace 'my_data' with the path to your file
+	    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+function readJSON(path) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) { 
+      if (this.status == 200) {
+          var file = new File([this.response], 'temp');
+          var fileReader = new FileReader();
+          fileReader.addEventListener('load', function(){
+               //do stuff with fileReader.result
+          });
+          fileReader.readAsText(file);
+      } 
+    }
+    xhr.send();
+}
 
 
-var output = document.getElementById('zone');
-output.innerHTML = texto.name;
+//var data;
+//
+$.getJSON('JS/info.json',function(data){
+	//console.log(data);
+	var output = document.getElementById('info');
+	output.innerHTML = data.hora;
+});
+
--- JS/funciones.js
+++ JS/funciones.js
@@ -2,3 +2,59 @@ var dt = new Date();
 var fecha = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
 var hora = {hour:'2-digit', minute:'2-digit'}
 document.getElementById("datetime").innerHTML = dt.toLocaleString("es-MX",fecha)+' / '+dt.toLocaleString("es-MX",hora)+' h';
+
+
+var swiped = false;
+function swipe(){
+	if(swiped){
+		document.getElementById("map").innerHTML = '<a onclick="swipe()"  onmouseover="" style="cursor: pointer;"><img style="height: 100%; width: 100%;" src="img/mapaTest.jpg" ></a>'
+		swiped= !swiped;
+//		console.log("nuevo valor de SWIPED: "+swiped);
+	}else{
+		document.getElementById("map").innerHTML = '<a onclick="swipe()"  onmouseover="" style="cursor: pointer;"><img style="height: 100%; width: 100%;" src="img/mapaTest2.gif" ></a>'
+		swiped= !swiped
+;	}
+}
+
+function editar(){
+document.getElementById("datetime").innerHTML = '<div class="well">	Fecha: <input type="text" name="date" class="datepicker" placeholder="Selecciona la fecha"/> '+
+												'Hora: <input type="text" id="time" placeholder="Selecciona la Hora"/> '+
+												'<button type="button" onclick="saveDate()" class="btn btn-outline-success">Guardar</button></div>'
+	
+	  $(function(){
+				newDate=$('.datepicker').datepicker({
+					language: "es",
+					days: true,
+					autoclose: true,
+					format: 'dd/mm/yyyy',
+					startDate: '-3d',
+					todayBtn: "linked",
+				    autoclose: true,
+				    todayHighlight: true
+				});
+			});
+	
+	 var nerHour;
+	 $(document).ready(function(){
+				$('#time').timepicker({
+					timeFormat: 'HH:mm',
+    				interval: 60,
+    				scrollbar: true,
+				});
+			});
+}
+
+function secretI(){
+	document.getElementById("secretButton").innerHTML = '<button type="button" class="btn btn-primary" onclick="editar()"><span class="glyphicon glyphicon-edit"></span> <ion-icon name="create"></ion-icon> Editar </button>'
+}
+function secretO(){
+	document.getElementById("secretButton").innerHTML = ' '
+}
+
+function saveDate(){
+	 var newDate = new Date();
+	 newDate= $(".datepicker").datepicker("getDate");
+	 newDate.setHours($('#time').timepicker("getTime").getHours());
+	 //console.log(newDate);
+	 document.getElementById("datetime").innerHTML = newDate.toLocaleString("es-MX",fecha)+' / '+newDate.toLocaleString("es-MX",hora)+' h';
+}
