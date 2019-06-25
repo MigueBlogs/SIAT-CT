daaf018affca067d4256a27e93831765994ce36f
MigueBogs <m_plqp2@hotmail.com>
2019-06-25 16:41:22 -0500

    Adjunto cambios de Luis
    Agregando cambios de Luis  y ya logré corregir los errores de Json ya que habia puesto demasiados pero ahora ya funciona bien. (Queda pendiente boton de guardar tabla)

--- JS/StaticText.js
+++ JS/StaticText.js
@@ -1,97 +1,95 @@
-//la base de datos va a caluclar e valor del numero de boletín?
-var texto = {"numero":"1"};
+$(function() {
+	//la base de datos va a caluclar e valor del numero de boletín?
+	var texto = {"numero":"1"};
+	var output = document.getElementById('Number');
+	output.innerHTML = texto.numero;
+
+	/*var infoGen={"hora": "17:30 (22:30 GMT)", 
+				"coords": "28.8°Norte 68.7° Oeste.",
+				"loc": "A 540 km al suroeste de Bermuda y a 2,010 km al este-noreste de las costas de Quintana Roo."
+				"despl": "Hacia el norte (350°) a 22km/h."
+				"viento": "65 km/h."
+				"racha":"85 km/h."};*/
+	function loadJSON(callback) {   
+		var xobj = new XMLHttpRequest();
+			xobj.overrideMimeType("application/json");
+			xobj.open('GET', 'JS/info.json', true); // Replace 'my_data' with the path to your file
+			xobj.onreadystatechange = function () {
+			if (xobj.readyState == 4 && xobj.status == "200") {
+				// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+				callback(xobj.responseText);
+			}
+		};
+		xobj.send(null);  
+	}
+
+	function readJSON(path) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', path, true);
+		xhr.responseType = 'blob';
+		xhr.onload = function(e) { 
+		if (this.status == 200) {
+			var file = new File([this.response], 'temp');
+			var fileReader = new FileReader();
+			fileReader.addEventListener('load', function(){
+				//do stuff with fileReader.result
+			});
+			fileReader.readAsText(file);
+		} 
+		}
+		xhr.send();
+	}
+	//Carga del evento
+	$.getJSON('JS/info.json',function(data){
+		//console.log(data);
+		var output = document.getElementById('NombreEvento');
+		output.innerHTML = data.NombreEvento;
+	});
+
+	$.getJSON('JS/info.json',function(data){
+		//console.log(data);
+		var output = document.getElementById('tipo');
+		output.innerHTML = data.tipo;
+	});
 
 
-var output = document.getElementById('Number');
-output.innerHTML = texto.numero;
+	//Cargando datos del Subtítulo
+	$.getJSON('JS/info.json',function(data){
+		//console.log(data);
+		//var output = document.getElementById('subtitle');
+		//output.innerHTML = data.texto;
+		document.getElementById("subtitle").value = data.texto;
+	});
 
-/*var infoGen={"hora": "17:30 (22:30 GMT)", 
-			"coords": "28.8°Norte 68.7° Oeste.",
-			"loc": "A 540 km al suroeste de Bermuda y a 2,010 km al este-noreste de las costas de Quintana Roo."
-			"despl": "Hacia el norte (350°) a 22km/h."
-			"viento": "65 km/h."
-			"racha":"85 km/h."};*/
-function loadJSON(callback) {   
 
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-	    xobj.open('GET', 'JS/info.json', true); // Replace 'my_data' with the path to your file
-	    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
+	//Cargando datos en zona de Información Gneral
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('hora');
+		output.innerHTML = data.hora;
+	});
 
-function readJSON(path) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', path, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e) { 
-      if (this.status == 200) {
-          var file = new File([this.response], 'temp');
-          var fileReader = new FileReader();
-          fileReader.addEventListener('load', function(){
-               //do stuff with fileReader.result
-          });
-          fileReader.readAsText(file);
-      } 
-    }
-    xhr.send();
-}
-//Carga del evento
-$.getJSON('JS/info.json',function(data){
-	//console.log(data);
-	var output = document.getElementById('NombreEvento');
-	output.innerHTML = data.NombreEvento;
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('coords');
+		output.innerHTML = data.coords;
+	});
+
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('loc');
+		output.innerHTML = data.loc;
+	});
+
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('despl');
+		output.innerHTML = data.despl;
+	});
+
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('viento');
+		output.innerHTML = data.viento;
+	});
+
+	$.getJSON('JS/info.json',function(data){
+		var output = document.getElementById('racha');
+		output.innerHTML = data.racha;
+	});
 });
-
-$.getJSON('JS/info.json',function(data){
-	//console.log(data);
-	var output = document.getElementById('tipo');
-	output.innerHTML = data.tipo;
-});
-
-
-//Cargando datos del Subtítulo
-$.getJSON('JS/info.json',function(data){
-	//console.log(data);
-	//var output = document.getElementById('subtitle');
-	//output.innerHTML = data.texto;
-	document.getElementById("subtitle").value = data.texto;
-});
-
-
-//Cargando datos en zona de Información Gneral
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('hora');
-	output.innerHTML = data.hora;
-});
-
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('coords');
-	output.innerHTML = data.coords;
-});
-
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('loc');
-	output.innerHTML = data.loc;
-});
-
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('despl');
-	output.innerHTML = data.despl;
-});
-
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('viento');
-	output.innerHTML = data.viento;
-});
-
-$.getJSON('JS/info.json',function(data){
-	var output = document.getElementById('racha');
-	output.innerHTML = data.racha;
-});
-
